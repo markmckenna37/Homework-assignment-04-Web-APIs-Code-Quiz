53f27578012d53ebc6a794b2ca83a6a27e16ed7d
Mark <youremail@gmail.comg>
2020-06-30 17:14:53 -0600

    reworked my question/answer variables into one big objects. the answer options to be an inner html attribute through JS

--- Develop/script.js
+++ Develop/script.js
@@ -7,14 +7,30 @@ var button2 = document.querySelector("#btn2");
 var button3 = document.querySelector("#btn3");
 var button4 = document.querySelector("#btn4");
 var questionEl = document.querySelector("#question")
+var buttonValue = -1;
+var i = 0;
+var questions = {
+    question1 : "Which of these statements is NOT a correct way to iterate a variable by 1:",
+    answer1 : ["A) i = i + 1", "B) i++", "C) i + i", "D) i += i"],
+    correctAnswer1 : 2,
+    question2 : "Which of these choices is NOT a data type:",
+    answer2 : ["A) string", "B) object", "C) boolean", "D) number"],
+    correctAnswer2 : 1,
+    question3 : "Which statement is NOT considered a 'string':",
+    answer3 : ["A) \"My name is Judge.\"", "B) \"1\"", "C) \"my name is\" + variable + \".\"", "D) 5000"],
+    correctAnswer3 : 3,
+    question4 : "Which word can NOT be used to make a variable?:",
+    answer4 : ["A) obj", "B) var", "C) const", "D) let"],
+    correctAnswer4 : 0,
+    question5 : "Which of these data types would be considered truthy?:",
+    answer5 : ["A) null", "B) 0", "C) NaN", "D) 1"],
+    correctAnswer5 : 3,
+    question6 : "What HTML <tag> would you use to link your javascript file?:",
+    answer6 : ["A) <meta>", "B) <script>", "C) <JS>", "D) <link>"],
+    correctAnswer6 : 1
+}
 
-// making a vaiable for every question cause I don't know what else to do yet.
-var Q1 = "Which of these statements is NOT a correct way to iterate a variable by 1:";
-var Q2 = "Which of these choices is NOT a data type:";
-var Q3 = "Which statement is NOT considered a 'string':";
-var Q4 = "Which word can NOT be used to make a variable?:";
-var Q5 = "Which of these data types would be considered truthy?:";
-var Q6 = "What HTML <tag> would you use to link your javascript file?:";
+
 // Set a timer starting at 75s and counts down by a the second
 // establish a time variable, set it to 75
 var timeLeft = 75;
@@ -38,113 +54,36 @@ function startQuiz() {
     choicesEl.style.display = "block";
     countDown();
 }
-// include an if statement to subtract 20 seconds from the timer if the question quiz question is answered incorrectly
+function giveQuestion1() {
+    for (var i = 0; i < questions.answer1.length; i++){ 
+    questionEl.textContent = questions.question1;   
+    var li = document.createElement("li");
+    li.setAttribute("data-index", i);
+    li.innerHTML = "<button>Choose</button> " + questions.answer1[i];
+    choicesEl.appendChild(li);
+    }
+}
+
+for (var i = 0; i < questions.answer1.length; i++){ 
+console.log(questions.answer3[i]);}
+
+    // include an if statement to subtract 20 seconds from the timer if the question quiz question is answered incorrectly
 
 // set all of the buttons up with a dynamic click event that tracks whether the answer is correct of incorrect
-choicesEl.addEventListener("click", function(event) {
-    event.preventDefault();
-    if (event.target.matches("button")) {
-        console.log("we in bidness");
-    }
-});
+
+
 // need to make it so the start button disappears after starting the quiz
 startBtn.addEventListener("click", startQuiz);
+startBtn.addEventListener("click", giveQuestion1);
 
 // make a few variables that contain all of the questions and answers
-var answerOptions = {
-    0 :  [
-        "A) i = i + 1",
-        "B) i++",
-        "C) i + i",
-        "D) i += i"
-    ],
-    1 : [
-        "A) string",
-        "B) object",
-        "C) boolean",
-        "D) number"
-    ],
-    2 : [
-        "A) \"My name is Judge.\"",
-        "B) \"1\"",
-        "C) \"my name is\" + variable + \".\"",
-        "D) 5000"
-    ],
-    3 : [
-        "A) obj",
-        "B) var",
-        "C) const",
-        "D) let"
-    ] ,
-    4 : [
-        "A) null",
-        "B) 0",
-        "C) NaN",
-        "D) 1"
-    ],
-    5 : [
-        "A) <meta>",
-        "B) <script>",
-        "C) <JS>",
-        "D) <link>"
-    ],
-}
-var answers = {
-    0 : 3,
-    1 : 2,
-    2 : 4,
-    3 : 1,
-    4 : 4,
-    5 : 2
-}
+
+
+
 
 // make a function that switches the content within the question field and answer buttons after a question is answered
-function giveQuestion() {
-    for (i = 0; i < 6; i++)
-    questionEl.textContent = questions[i];
-}
+
+
+    
+
 // After the quiz is finished, load the high score and player's initial to local storage
-console.log(answerOptions[2][2])
-
-// gonna write my questions now cause I'm stuck
-
-// Which of these statements is NOT a correct way to iterate a variable by 1:
-// A i = i + 1
-// 2) i++
-// 3) i + i
-// 4) i += i
-
-// Which of these choices is NOT a data type:
-// 1) string
-// 2) object
-// 3) boolean
-// 4) number
-
-// Which statement is NOT considered a "string":
-// 1) "my name is judge"
-// 2) "1"
-// 3) "my name is" + variable + "."
-// 4) 5000
-
-// Which word can NOT be used to make a variable?:
-// 1) obj 
-// 2) var
-// 3) const
-// 4) let
-
-// Which of these data types would be considered truthy?:
-// 1) null 
-// 2) 0
-// 3) NaN
-// 4) 1
-
-// What HTML <tag> would you use to link your javascript file?:
-// 1) <meta>
-// 2) <script>
-// 3) <JS>
-// 4) <link>
-
-// "A)"
-// "B)"
-// "C)"
-// "D)"
