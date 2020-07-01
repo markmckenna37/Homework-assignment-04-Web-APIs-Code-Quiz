8c019c4b860a87deec0c210d5d9ef8873064e82e
Mark <youremail@gmail.comg>
2020-07-01 14:59:02 -0600

    went back and changed up a lot of HTML elements and JS variables. Added an  array of objects and 3 main div containers for HTML elements

--- Develop/script.js
+++ Develop/script.js
@@ -2,33 +2,38 @@
 var timeEl = document.querySelector("#timeLeft");
 var choicesEl = document.querySelector("#choices");
 var startBtn = document.querySelector("#startBtn");
-var button1 = document.querySelector("#btn1");
-var button2 = document.querySelector("#btn2");
-var button3 = document.querySelector("#btn3");
-var button4 = document.querySelector("#btn4");
 var questionEl = document.querySelector("#question")
-var buttonValue = -1;
 var i = 0;
-var questions = {
-    question1 : "Which of these statements is NOT a correct way to iterate a variable by 1:",
-    answer1 : ["A) i = i + 1", "B) i++", "C) i + i", "D) i += i"],
-    correctAnswer1 : 2,
-    question2 : "Which of these choices is NOT a data type:",
-    answer2 : ["A) string", "B) object", "C) boolean", "D) number"],
-    correctAnswer2 : 1,
-    question3 : "Which statement is NOT considered a 'string':",
-    answer3 : ["A) \"My name is Judge.\"", "B) \"1\"", "C) \"my name is\" + variable + \".\"", "D) 5000"],
-    correctAnswer3 : 3,
-    question4 : "Which word can NOT be used to make a variable?:",
-    answer4 : ["A) obj", "B) var", "C) const", "D) let"],
-    correctAnswer4 : 0,
-    question5 : "Which of these data types would be considered truthy?:",
-    answer5 : ["A) null", "B) 0", "C) NaN", "D) 1"],
-    correctAnswer5 : 3,
-    question6 : "What HTML <tag> would you use to link your javascript file?:",
-    answer6 : ["A) <meta>", "B) <script>", "C) <JS>", "D) <link>"],
-    correctAnswer6 : 1
-}
+var index;
+// Changed all of the questions/answers objects into an array of objects with question, answer PushSubscriptionOptions, and correct answer
+
+var questions = [{
+        question: "Which of these statements is NOT a correct way to iterate a variable by 1:",
+        answer: ["A) i = i + 1", "B) i++", "C) i + i", "D) i += i"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of these choices is NOT a data type:",
+        answer: ["A) string", "B) object", "C) boolean", "D) number"],
+        correctAnswer: 1,
+    }, {
+        question: "Which statement is NOT considered a 'string':",
+        answer: ["A) \"My name is Judge.\"", "B) \"1\"", "C) \"my name is\" + variable + \".\"", "D) 5000"],
+        correctAnswer: 3,
+    }, {
+        question: "Which word can NOT be used to make a variable?:",
+        answer: ["A) obj", "B) var", "C) const", "D) let"],
+        correctAnswer: 0,
+    }, {
+        question: "Which of these data types would be considered truthy?:",
+        answer: ["A) null", "B) 0", "C) NaN", "D) 1"],
+        correctAnswer: 3,
+    }, {
+        question: "What HTML <tag> would you use to link your javascript file?:",
+        answer: ["A) <meta>", "B) <script>", "C) <JS>", "D) <link>"],
+        correctAnswer: 1
+    }
+]
 
 
 // Set a timer starting at 75s and counts down by a the second
@@ -38,11 +43,11 @@ var timeLeft = 75;
 choicesEl.style.display = "none";
 // write a function to count down from by seconds from 75.
 function countDown() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         timeLeft--;
-        timeEl.textContent = timeLeft ;
+        timeEl.textContent = timeLeft;
 
-        if(timeLeft === 0) {
+        if (timeLeft <= 0) {
             clearInterval(timerInterval);
             alert("OUT OF TIME")
         }
@@ -54,27 +59,138 @@ function startQuiz() {
     choicesEl.style.display = "block";
     countDown();
 }
-function giveQuestion1() {
-    for (var i = 0; i < questions.answer1.length; i++){ 
-    questionEl.textContent = questions.question1;   
+
+function giveQuestion() {
+    for (var i = 0; i < questions[i].answer.length; i++) {
     var li = document.createElement("li");
     li.setAttribute("data-index", i);
-    li.innerHTML = "<button>Choose</button> " + questions.answer1[i];
+    li.innerHTML = "<button>Choose</button> " + questions[i].answer[i];
     choicesEl.appendChild(li);
     }
+    // for (var i = 0; i < questions.length; i++) {
+
+    // }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if (el.matches("button")) {
+            console.log("nutsack")
+        }
+        // if ((el.matches("button")) && (index !== 2)) {
+        //     timeLeft -= 20;
+        // }
+    })
 }
 
-for (var i = 0; i < questions.answer1.length; i++){ 
-console.log(questions.answer3[i]);}
+function giveQuestion2() {
+    var li = document.getElementById("li");
+    questionEl.textContent = questions.question2;
+    for (var i = 0; i < 4; i++) {
+        var li = document.getElementById("");
+        li.textContent = "Choose " + questions.answer2[i];
+    }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if ((el.matches("button")) && (index === 1)) {
+            giveQuestion3();
+        }
+        if ((el.matches("button")) && (index !== 1)) {
+            timeLeft -= 20;
+        }
+    })
+}
 
-    // include an if statement to subtract 20 seconds from the timer if the question quiz question is answered incorrectly
+function giveQuestion3() {
+    for (var i = 0; i < 4; i++) {
+        questionEl.textContent = questions.question3;
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+        li.innerHTML = "<button>Choose</button> " + questions.answer3[i];
+        choicesEl.appendChild(li);
+    }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if ((el.matches("button")) && (index === 3)) {
+            giveQuestion4();
+        }
+        if ((el.matches("button")) && (index !== 3)) {
+            timeLeft -= 20;
+        }
+    })
+}
+
+function giveQuestion4() {
+    for (var i = 0; i < 4; i++) {
+        questionEl.textContent = questions.question4;
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+        li.innerHTML = "<button>Choose</button> " + questions.answer4[i];
+        choicesEl.appendChild(li);
+    }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if ((el.matches("button")) && (index === 0)) {
+            giveQuestion5();
+        }
+        if ((el.matches("button")) && (index !== 0)) {
+            timeLeft -= 20;
+        }
+    })
+}
+
+function giveQuestion5() {
+    for (var i = 0; i < 4; i++) {
+        questionEl.textContent = questions.question5;
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+        li.innerHTML = "<button>Choose</button> " + questions.answer5[i];
+        choicesEl.appendChild(li);
+    }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if ((el.matches("button")) && (index === 3)) {
+            giveQuestion6();
+        }
+        if ((el.matches("button")) && (index !== 3)) {
+            timeLeft -= 20;
+        }
+    })
+}
+
+function giveQuestion6() {
+    for (var i = 0; i < 4; i++) {
+        questionEl.textContent = questions.question6;
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+        li.innerHTML = "<button>Choose</button> " + questions.answer6[i];
+        choicesEl.appendChild(li);
+    }
+    choicesEl.addEventListener("click", function (event) {
+        var el = event.target;
+        var index = el.getAttribute("data-index");
+        if ((el.matches("button")) && (index === 1)) {
+            giveQuestion2();
+        }
+        if ((el.matches("button")) && (index !== 1)) {
+            timeLeft -= 20;
+        }
+    })
+}
+
+
+
+// include an if statement to subtract 20 seconds from the timer if the question quiz question is answered incorrectly
 
 // set all of the buttons up with a dynamic click event that tracks whether the answer is correct of incorrect
 
 
 // need to make it so the start button disappears after starting the quiz
 startBtn.addEventListener("click", startQuiz);
-startBtn.addEventListener("click", giveQuestion1);
+startBtn.addEventListener("click", giveQuestion);
 
 // make a few variables that contain all of the questions and answers
 
@@ -84,6 +200,6 @@ startBtn.addEventListener("click", giveQuestion1);
 // make a function that switches the content within the question field and answer buttons after a question is answered
 
 
-    
+
 
 // After the quiz is finished, load the high score and player's initial to local storage
