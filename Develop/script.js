e44fad8b04f90dfa8a75e48b762934a99602af2e
Mark <youremail@gmail.comg>
2020-07-01 17:05:14 -0600

    tied the givequestion function to the parameter of current question so itterated variable can pull each question from our questions array

--- Develop/script.js
+++ Develop/script.js
@@ -63,31 +63,27 @@ function startQuiz() {
     startPage.style.display = "none";
     quizPage.style.display = "block";
     countDown();
-    giveQuestion()
+    giveQuestion(currentQuestion)
 }
 // function that gives current question and answer options. the current question is pulled from the questions array using the current question function
-function giveQuestion() {
-        questionEl.textContent = questions[0].question
-        button0.textContent = questions[0].answer[0]
-        button1.textContent = questions[0].answer[1]
-        button2.textContent = questions[0].answer[2]
-        button3.textContent = questions[0].answer[3]
+function giveQuestion(i) {
+        questionEl.textContent = questions[i].question
+        button0.textContent = questions[i].answer[0]
+        button1.textContent = questions[i].answer[1]
+        button2.textContent = questions[i].answer[2]
+        button3.textContent = questions[i].answer[3]
 }
 
 // function that is triggered on click with each of the 4 buttons. each button corresponds to a number 0-4, 
 // function checkAnswer(answer) {
 //     // the number associated with each button is then compared to the correct answer of the current question, from the questions array 
 // can't figure that out, so I made it a different function for each button
-//  if (answer === questions[currentQuestion].correctAnswer){
-//      currentQuestion++
-// }
-// }
 
 
 
 
 
-// include an if statement to subtract 20 seconds from the timer if the question quiz question is answered incorrectly
+
 
 // set all of the buttons up with a dynamic click event that tracks whether the answer is correct of incorrect
 
@@ -98,27 +94,34 @@ startBtn.addEventListener("click", startQuiz);
 button0.addEventListener("click", function() {
     // takes the current question's correct answer value and compares it to numbers 0-3
     if (0 === questions[currentQuestion].correctAnswer) {
-        console.log("cool");
+        // adding 1 to our current questions variable and calling the next function with the currentquestion value
         currentQuestion++
+        giveQuestion(currentQuestion)
     }
+    // if the wrong button is clicked, 20 seconds is deducted from the timeleft variable
+    else {timeLeft -= 20}
 });
+// repeating for the other 3 buttons
 button1.addEventListener("click", function() {
     if (1 === questions[currentQuestion].correctAnswer) {
-        console.log("cool");
         currentQuestion++
+        giveQuestion(currentQuestion)
     }
+    else {timeLeft -= 20}
 });
 button2.addEventListener("click", function() {
     if (2 === questions[currentQuestion].correctAnswer) {
-        console.log("coool");
         currentQuestion++
+        giveQuestion(currentQuestion)
     }
+    else {timeLeft -= 20}
 });
 button3.addEventListener("click", function() {
     if (3 === questions[currentQuestion].correctAnswer) {
-        console.log("coooooool");
         currentQuestion++
+        giveQuestion(currentQuestion)
     }
+    else {timeLeft -= 20}
 });
 // make a few variables that contain all of the questions and answers
 
